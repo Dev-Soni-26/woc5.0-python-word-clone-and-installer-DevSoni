ee04010fdccb3f6309f2af4ad34dbc1d496980da
Dev-Soni-26 <122281667+Dev-Soni-26@users.noreply.github.com>
2023-01-15 20:04:14 +0530

    Lecture 1 Python Code
    In this code, i experimented with comments and variables in python!!

---
+++ lec1.py
@@ -0,0 +1,23 @@
+# This code is for wrting comments, and some simple variable statements in python
+print("Hello World")
+
+#using an integer variable in python
+x=10;
+print(x)
+
+#strings
+name_string = "Dev Soni"
+print(name_string)
+
+#unpacking 
+days = {"mon", "tue", "wed"}
+#overwrting x
+x,y,z = days
+print(x, y, z)
+
+
+#multiline comment 
+"""
+This is a multiline comment 
+consisting of 2 lines
+"""
